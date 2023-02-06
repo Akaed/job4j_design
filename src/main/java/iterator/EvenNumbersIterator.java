0e6cfe8edea6b487266593d2ab2b631c9afd16da
Ilya <ilyayurch@gmail.com>
2023-02-06 03:54:01 +0300

    5.1.2.Итератор четных чисел [#150 #388390]

--- src/main/java/iterator/EvenNumbersIterator.java
+++ src/main/java/iterator/EvenNumbersIterator.java
@@ -6,7 +6,6 @@ import java.util.NoSuchElementException;
 public class EvenNumbersIterator implements Iterator<Integer> {
     private int[] data;
     private  int index;
-    private int count;
 
     public EvenNumbersIterator(int[] data) {
         this.data = data;
@@ -14,15 +13,10 @@ public class EvenNumbersIterator implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-       boolean countboolean = false;
-        for (int i = index; i < data.length; i++) {
-            if (data[i] % 2 == 0) {
-                index = i;
-                countboolean = true;
-                break;
-            }
-        }
-        return countboolean;
+       while (index < data.length && data[index] % 2 != 0) {
+           index++;
+       }
+        return index < data.length;
     }
 
     @Override
@@ -30,8 +24,6 @@ public class EvenNumbersIterator implements Iterator<Integer> {
             if (!hasNext()) {
                 throw new NoSuchElementException();
             }
-
-
         return data[index++];
     }
 }

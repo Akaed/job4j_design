69e592c8fbe90521d0a528852404c40fdacfeb98
Ilya <ilyayurch@gmail.com>
2023-03-29 23:13:14 +0400

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/collection/SimpleArrayList.java
@@ -0,0 +1,77 @@
+package collection;
+
+import java.util.*;
+
+public class SimpleArrayList <T> implements SimpleList<T> {
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            container = grow();
+        }
+        container[size] = value;
+        modCount++;
+        size++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, size);
+        T rls = container[index];
+        container[index] = newValue;
+        return rls;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        T rsl = container[index];
+        System.arraycopy(container, index + 1, container, index, container.length - index - 1);
+        size--;
+        modCount++;
+        return rsl;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return  container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int expectedModCount = modCount;
+            int count;
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return count < size;
+            }
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[count++];
+            }
+        };
+    }
+    private T[] grow() {
+       return Arrays.copyOf(container, (container.length + 1) * 2);
+    }
+}

74b3484b3fe6ddd43375ee4a31749b29b3cc2287
Ilya <ilyayurch@gmail.com>
2023-05-17 05:09:53 +0400

    Реализовать коллекцию Set на массиве [#996]

---
+++ src/main/java/set/SimpleSet.java
@@ -0,0 +1,28 @@
+package set;
+
+import collection.SimpleArrayList;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Objects;
+
+public class SimpleSet<T> implements Set<T> {
+    private static final Object PRESENT = new Object();
+    private SimpleArrayList<T> set = new SimpleArrayList<>(0);
+    private HashMap<T,T> map = new HashMap();
+
+    @Override
+    public boolean add(T value) {
+        return map.put(value, (T) PRESENT) == null;
+    }
+
+    @Override
+    public boolean contains(T value) {
+        return Objects.equals(set.iterator(), value);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return set.iterator();
+    }
+}
